068fe8dc0a94f642f5e95cb04aed33b79a5c27e9
nattaditjoe <nattaditjoe@gmail.com>
2025-11-13 13:54:11 +0700

    V2

---
+++ hr-template-library/src/index.tsx
@@ -0,0 +1,1 @@
+/* Paste your full index.tsx from canvas here */
---
+++ hr-template-library/src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HRTemplateLibrary from "./index";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode><HRTemplateLibrary /></React.StrictMode>
+);
